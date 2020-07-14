6869120e86f3893fd173a3aea5053511cf8c0e60
Krantz-XRF <Krantz.XRF@outlook.com>
2020-07-14 23:45:27 +0800

    [Logic] Bug fix: correctly handle move Down

--- 2048IsHard/src/A2048/Logic.hs
+++ 2048IsHard/src/A2048/Logic.hs
@@ -79,10 +79,17 @@ rotateTo Right = map reverse
 rotateTo Up    = transpose
 rotateTo Down  = map reverse . transpose
 
+-- |Recover from a rotated game board according to a 'Direction'.
+rotateFrom :: Direction -> [[a]] -> [[a]]
+rotateFrom Left  = id
+rotateFrom Right = map reverse
+rotateFrom Up    = transpose
+rotateFrom Down  = transpose . map reverse
+
 -- |Gather game events on a game move to a given 'Direction'.
 events :: Int -> Int -> Direction -> [[Int]] -> [[GameEvent Int (Int, Int)]]
 events w h d = rotated (transIndices . boardMerge) . mapInput where
   mapInput = mapBoard $ \case 0 -> Nothing; n -> Just n
   transIndices = mapBoard $ second $ idxTrans w h d
-  rotated f = rotateTo d . f . rotateTo d
+  rotated f = rotateFrom d . f . rotateTo d
   mapBoard f = map (map f)
