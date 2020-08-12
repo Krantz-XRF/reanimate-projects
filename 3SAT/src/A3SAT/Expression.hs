e75f779b7725e47de083e094f9e1e3dcfdfa6568
Krantz-XRF <Krantz.XRF@outlook.com>
2020-08-12 19:07:31 +0800

    [3SAT] lenses

--- 3SAT/src/A3SAT/Expression.hs
+++ 3SAT/src/A3SAT/Expression.hs
@@ -10,9 +10,9 @@ Portability : portable
 {-# LANGUAGE TemplateHaskell #-}
 module A3SAT.Expression where
 
-import Data.List
 import Data.Bifunctor
 import Data.Bifunctor.Fix
+import Data.List
 
 import Data.Bifunctor.TH
 import Data.Bitraversable
@@ -24,13 +24,13 @@ import Text.Show.Deriving
 
 -- ** Expression Functors with Labels
 
--- |The endofunctor for boolean expressions. 
+-- |The endofunctor for boolean expressions.
 data ExpressionF a l
-  = LVarF l Word
-  | LNotF l a
-  | LAndF a l a
-  | LOrF a l a
-  | LParenF l a l
+  = VarF l Word
+  | NotF l a
+  | AndF a l a
+  | OrF a l a
+  | ParenF l a l
   deriving stock (Show, Eq, Functor, Foldable, Traversable)
 
 deriveBifunctor ''ExpressionF
@@ -43,41 +43,57 @@ deriveShow2 ''ExpressionF
 
 -- |Given number of glyphs for variables, get the number of glyphs for this element.
 glyphCount :: [Int] -> ExpressionF a l -> Int
-glyphCount g (LVarF _ x) = g `genericIndex` x
-glyphCount _ (LNotF _ _) = 1
-glyphCount _ (LAndF _ _ _) = 1
-glyphCount _ (LOrF _ _ _) = 1
-glyphCount _ (LParenF _ _ _) = 1
+glyphCount g (VarF _ x)     = g `genericIndex` x
+glyphCount _ (NotF _ _)     = 1
+glyphCount _ (AndF _ _ _)   = 1
+glyphCount _ (OrF _ _ _)    = 1
+glyphCount _ (ParenF _ _ _) = 1
 
 -- ** Expression Constructors with Labels
 
+-- |Labeled Variable.
 pattern LVar :: l -> Word -> LExpression l
-pattern LVar l x = In (LVarF l x)
+pattern LVar l x = In (VarF l x)
 
+-- |Boolean negation with label.
 pattern LNot :: l -> LExpression l -> LExpression l
-pattern LNot l a = In (LNotF l a)
+pattern LNot l a = In (NotF l a)
 
-pattern LAnd, LOr :: LExpression l -> l -> LExpression l -> LExpression l
-pattern LAnd a l b = In (LAndF a l b)
-pattern LOr a l b = In (LOrF a l b)
+-- |Boolean and with label.
+pattern LAnd :: LExpression l -> l -> LExpression l -> LExpression l
+pattern LAnd a l b = In (AndF a l b)
 
+-- |Boolean or with label.
+pattern LOr :: LExpression l -> l -> LExpression l -> LExpression l
+pattern LOr a l b = In (OrF a l b)
+
+-- |Parenthesis with left and right labels.
 pattern LParen :: l -> LExpression l -> l -> LExpression l
-pattern LParen l a r = In (LParenF l a r)
+pattern LParen l a r = In (ParenF l a r)
 
 {-# COMPLETE LVar, LNot, LAnd, LOr, LParen #-}
 
 -- ** Expression Constructors without Labels
 
+-- |Variable with a dummy label.
 pattern Var :: Word -> Expression
 pattern Var x = LVar () x
 
-pattern Not, Paren :: Expression -> Expression
+-- |Boolean negation with a dummy label.
+pattern Not :: Expression -> Expression
 pattern Not a = LNot () a
-pattern Paren a = LParen () a ()
 
-pattern And, Or :: Expression -> Expression -> Expression
+-- |Boolean and with a dummy label.
+pattern And :: Expression -> Expression -> Expression
 pattern And a b = LAnd a () b
+
+-- |Boolean or with a dummy label.
+pattern Or :: Expression -> Expression -> Expression
 pattern Or a b = LOr a () b
+
+-- |Parenthesis with dummy labels.
+pattern Paren :: Expression -> Expression
+pattern Paren a = LParen () a ()
 
 {-# COMPLETE Var, Not, And, Or, Paren #-}
 
@@ -99,14 +115,14 @@ type Expression = LExpression ()
 addParenWith :: (l, l) -> LExpression l -> LExpression l
 addParenWith (l, r) = go (0 :: Int) where
   go p e@(priority . out -> p')
-    | p' <= p = In (LParenF l e' r)
+    | p' <= p = In (ParenF l e' r)
     | otherwise = e'
     where e' = In $ first (go p') $ out e
-  priority (LVarF _ _) = 4
-  priority (LNotF _ _) = 2
-  priority (LAndF _ _ _) = 1
-  priority (LOrF _ _ _) = 1
-  priority (LParenF _ _ _) = 3
+  priority (VarF _ _)     = 4
+  priority (NotF _ _)     = 2
+  priority (AndF _ _ _)   = 1
+  priority (OrF _ _ _)    = 1
+  priority (ParenF _ _ _) = 3
 
 -- |Add necessary parentheses for the boolean expression.
 addParen :: Expression -> Expression
@@ -120,3 +136,25 @@ mapExprM :: Applicative f
          -> f (LExpression d)
 mapExprM convLabel = go where
   go (In e) = In <$> bimapM go (convLabel e) e
+
+-- |Zip 2 expression functors with of same shape with given functions.
+zipExprFWith :: (a -> a' -> a'') -> (l -> l' -> l'')
+             -> ExpressionF a l -> ExpressionF a' l' -> ExpressionF a'' l''
+zipExprFWith _ g (VarF l1 x)       (VarF l2 _)       = VarF (g l1 l2) x
+zipExprFWith f g (NotF l1 a1)      (NotF l2 a2)      = NotF (g l1 l2) (f a1 a2)
+zipExprFWith f g (AndF a1 l1 b1)   (AndF a2 l2 b2)   = AndF (f a1 a2) (g l1 l2) (f b1 b2)
+zipExprFWith f g (OrF a1 l1 b1)    (OrF a2 l2 b2)    = OrF (f a1 a2) (g l1 l2) (f b1 b2)
+zipExprFWith f g (ParenF l1 a1 r1) (ParenF l2 a2 r2) = ParenF (g l1 l2) (f a1 a2) (g r1 r2)
+zipExprFWith _ _ _ _ = error "zipExprFWith: requires input of same shape"
+
+-- |Zip 2 expression functors of the same shape.
+zipExprF :: ExpressionF a l -> ExpressionF a' l' -> ExpressionF (a, a') (l, l')
+zipExprF = zipExprFWith (,) (,)
+
+-- |Zip 2 expressions of the same shape with given function.
+zipExprWith :: (l -> l' -> l'') -> LExpression l -> LExpression l' -> LExpression l''
+zipExprWith f = go where go (In x) (In y) = In (zipExprFWith go f x y)
+
+-- |Zip 2 expressions of the same shape with given function.
+zipExpr :: LExpression l -> LExpression l' -> LExpression (l, l')
+zipExpr = zipExprWith (,)
---
+++ 3SAT/src/A3SAT/Expression/Lens.hs
@@ -0,0 +1,109 @@
+{-|
+Module      : A3SAT.Expression.Lens
+Description : Lens for the 'ExpressionF' functor and 'LExpression's.
+Copyright   : (c) Xie Ruifeng, 2020
+License     : AGPL-3
+Maintainer  : krantz.xrf@outlook.com
+Stability   : experimental
+Portability : portable
+-}
+{-# LANGUAGE TemplateHaskell #-}
+module A3SAT.Expression.Lens where
+
+import A3SAT.Expression
+import Control.Lens
+import Control.Lens.Extras (is)
+import Data.Bifunctor.Fix
+import Data.Bitraversable
+
+-- * Optics for Expression Functor
+
+makePrisms ''ExpressionF
+
+-- |Prisms for 'AndF' and 'OrF'.
+_BinOpF :: Traversal' (ExpressionF a l) (a, l, a)
+_BinOpF = disjointUnion _AndF _OrF
+
+-- |Label for variables matching the predicate.
+varF :: (Word -> Bool) -> Traversal' (ExpressionF a l) (l, Word)
+varF f = _VarF . filtered (f . view _2)
+
+-- |Label for 'NotF'.
+notF :: Traversal' (ExpressionF a l) l
+notF = _NotF . _1
+
+-- |Label for 'AndF'.
+andF :: Traversal' (ExpressionF a l) l
+andF = _AndF . _2
+
+-- |Label for 'OrF'.
+orF :: Traversal' (ExpressionF a l) l
+orF = _OrF . _2
+
+-- |Label for 'AndF' and 'OrF'.
+binOpF :: Traversal' (ExpressionF a l) l
+binOpF = _BinOpF . _2
+
+-- |Left parentheses.
+leftParenF :: Traversal' (ExpressionF a l) l
+leftParenF = _ParenF . _1
+
+-- |Right parentheses.
+rightParenF :: Traversal' (ExpressionF a l) l
+rightParenF = _ParenF . _3
+
+-- |Both parentheses.
+parenF :: Traversal' (ExpressionF a l) l
+parenF = _ParenF . _1_and_3 where
+  _1_and_3 f (x, a, y) = (,,) <$> f x <*> pure a <*> f y
+
+-- * Optics for Expressions
+
+-- |Label for variables matching the predicate.
+vars :: (Word -> Bool) -> Traversal' (LExpression l) (l, Word)
+vars f = fixExpr (varF f)
+
+-- |Label for 'NotF'.
+nots :: Traversal' (LExpression l) l
+nots = fixExpr notF
+
+-- |Label for 'AndF'.
+ands :: Traversal' (LExpression l) l
+ands = fixExpr andF
+
+-- |Label for 'OrF'.
+ors :: Traversal' (LExpression l) l
+ors = fixExpr orF
+
+-- |Label for 'AndF' and 'OrF'.
+binOps :: Traversal' (LExpression l) l
+binOps = fixExpr binOpF
+
+-- |Left parentheses.
+leftParens :: Traversal' (LExpression l) l
+leftParens = fixExpr leftParenF
+
+-- |Right parentheses.
+rightParens :: Traversal' (LExpression l) l
+rightParens = fixExpr rightParenF
+
+-- |Both parentheses.
+parens :: Traversal' (LExpression l) l
+parens = fixExpr parenF
+
+-- * Utility Functions
+
+-- |Make a disjoint union of 2 'Prism''s.
+disjointUnion :: Prism' s a -> Prism' s a -> Traversal' s a
+disjointUnion l r f s = if is l s then l f s else r f s
+
+-- |Fixpoint for lenses on the expression functor.
+--
+-- WARNING: recursive traversal are done after non-recursive ones.
+-- Thus traversal does not follow the field order in data declaration.
+fixExpr :: Applicative f
+        => (forall a . LensLike' f (ExpressionF a l) b)
+        -> LensLike' f (LExpression l) b
+fixExpr ls f = go where
+  go (In x) = In <$> (merge <$> ls f x <*> bitraverse go pure x)
+  merge = zipExprFWith const (const id)
