5646c833e7177fc00dbf5757fa1a7aef263e54b2
Krantz-XRF <Krantz.XRF@outlook.com>
2020-08-13 00:18:50 +0800

    [3SAT] simple animation primitives - expression -> svg - colour variables - colour other glyphs by category

---
+++ 3SAT/src/A3SAT/Animation.hs
@@ -0,0 +1,52 @@
+{-|
+Module      : A3SAT.Animation
+Description : Basic animations for 3-SAT problem.
+Copyright   : (c) Xie Ruifeng, 2020
+License     : AGPL-3
+Maintainer  : krantz.xrf@outlook.com
+Stability   : experimental
+Portability : portable
+-}
+{-# LANGUAGE OverloadedStrings #-}
+module A3SAT.Animation where
+
+import qualified Data.Text as T
+
+import Control.Lens
+import Data.List
+
+import A3SAT.Expression
+import A3SAT.Expression.Lens
+import A3SAT.LaTeX
+
+import Graphics.SvgTree
+import Reanimate
+
+-- |Render the boolean expression with LaTeX, then associate nodes with glyphs.
+showSvg :: [T.Text] -> LExpression l -> LExpression (l, SVG)
+showSvg xs e = associateGlyphs glyphs (map countGlyphs xs) e
+  where countGlyphs = length . svgGlyphs . mkTex
+        glyphs = [ f a | (f, _, a) <- svgGlyphs $ mkTex $ showLaTeX xs e ]
+        mkTex = withFillColor "white" . latex . mkMath
+        mkMath x = "$" <> x <> "$"
+
+-- |Modify the variables.
+--
+-- @'mapVar' idx attrs filt upd@ does the following:
+--
+-- * Locate all the variables using 'vars', filtered by @filt@
+-- * Focus on the targeted parts with the given 'Lens'' @idx@
+-- * Modify that part with @upd@, provided the corresponding attribute
+--   in @attrs@, indexed by the 'Word' index of that variable
+mapVar :: Lens' l b -> [a] -> (Word -> Bool) -> (a -> b -> b) -> LExpression l -> LExpression l
+mapVar idx attrs filt upd = over (vars filt) updVar
+  where updVar (l, n) = (over idx (upd (attrs `genericIndex` n)) l, n)
+
+-- |Colour the variables.
+colourVar :: Lens' l SVG -> [Texture] -> LExpression l -> LExpression l
+colourVar idx cols = mapVar idx cols (const True) upd
+  where upd c = fillColor .~ pure c
+
+-- |Colour labels by category.
+colourLabel :: Lens' (LExpression l) SVG -> Texture -> LExpression l -> LExpression l
+colourLabel idx col = over idx (fillColor .~ pure col)
