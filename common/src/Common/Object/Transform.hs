79ea536fe0c424b2ad0e5f1d840a260bef4261db
Xie Ruifeng <Krantz.XRF@outlook.com>
2020-12-09 11:40:34 +0800

    key frame: allow transforming groups of objects

--- common/src/Common/Object/Transform.hs
+++ common/src/Common/Object/Transform.hs
@@ -7,6 +7,7 @@ Maintainer  : krantz.xrf@outlook.com
 Stability   : experimental
 Portability : portable
 -}
+{-# LANGUAGE BlockArguments #-}
 module Common.Object.Transform
   (
   -- * Renderable Objects
@@ -15,6 +16,7 @@ module Common.Object.Transform
   -- * Alignment
   -- | Alignment controls how the objects in the next key frame is aligned according to
   --   those objects from the current frame.
+  , GroupAlignment
   -- ** X Alignment
   , leftAligned
   , rightAligned
@@ -24,13 +26,17 @@ module Common.Object.Transform
   , topAligned
   , yCentered
   -- * Key Frame Animations
-  , Trans(..)
+  , Trans(.., (:=>))
   , transformObject
   ) where
+
+import           Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as N
 
 import Common.Linear (Linear (lerp))
 
 import Control.Lens
+import Control.Monad
 
 import Data.Functor         (($>))
 import Data.Functor.Compose (Compose (..))
@@ -57,8 +63,12 @@ data Trans a b
   -- |Previous object vanishing in this frame.
   | Vanish a
   -- |Object transformaton in this frame.
-  | a :=> b
+  | (NonEmpty a) ::=> b
   deriving stock (Show, Functor, Foldable, Traversable)
+
+-- |Short cut for single object transformaton in this frame.
+pattern (:=>) :: a -> b -> Trans a b
+pattern a :=> b = (a :| []) ::=> b
 
 instance IsString b => IsString (Trans a b) where
   fromString = New . fromString
@@ -70,12 +80,13 @@ class SceneRender a where
   -- e.g. Group of 'T.Text' can be rendered using 'Reanimate.LaTeX.latexChunks'.
   renderGroup :: Traversable t => t a -> Scene s (t (Object s SVG))
 
-collectOld :: [Trans a b] -> [a]
-collectOld = mapMaybe $ \case
+collectOld :: [Trans a b] -> Compose [] NonEmpty a
+collectOld = Compose . mapMaybe \case
   New _    -> Nothing
-  Vanish x -> Just x
-  x :=> _  -> Just x
+  Vanish x -> Just (x :| [])
+  x ::=> _ -> Just x
 
+-- |Group alignment, given a base, align the second parameter according to the base.
 type GroupAlignment t t' s a b = t (Object s a) -> t' (Object s b) -> Scene s ()
 
 readCenterOf :: Traversable t
@@ -99,10 +110,10 @@ centeredOf :: (Traversable t, Traversable t')
            -> (forall x . Setter' (ObjectData x) Double)
            -> GroupAlignment t t' s a b
 centeredOf readCenter setPos base xs = do
-  xCenter <- readCenter base
-  xOrig <- readCenter xs
-  let deltaX = xCenter - xOrig
-  mapM_ (`oModify` (setPos %~ (+ deltaX))) xs
+  aCenter <- readCenter base
+  aOrig <- readCenter xs
+  let delta = aCenter - aOrig
+  mapM_ (`oModify` (setPos %~ (+ delta))) xs
 
 alignedOf :: (Traversable t, Traversable t')
           => (forall x . Getter (ObjectData x) Double)
@@ -150,13 +161,14 @@ applyAlignment align base xs = mapM_ (($ xs) . ($ base)) align $> xs
 --
 -- Warning: if alignment base (see 'Trans' for details) is empty, then all the
 -- alignment functions provided in this module would crash your program. In this
--- case, custom (ad-hoc) alignment functions might be useful.
+-- case, custom (ad-hoc) alignment functions might be useful. Or, to specify a
+-- custom alignment base @bs@, use @const (alignment bs)@ instead of @alignment@.
 --
 -- Note: if the target object utilise @OverloadedStrings@ via 'IsString', the type
 -- becomes ambiguous. Use @TypeApplications@ for this situation:
 -- > transformObject @ObjType alignments transformations
 transformObject :: SceneRender t
-                => [GroupAlignment [] (Compose [] (Trans (Object s a))) s a SVG]
+                => [GroupAlignment (Compose [] NonEmpty) (Compose [] (Trans (Object s a))) s a SVG]
                 -> [Trans (Object s a) t]
                 -> Scene s [Object s SVG]
 transformObject align xs
@@ -165,14 +177,17 @@ transformObject align xs
   >>= fmap catMaybes . mapM (fork . \case
     New b     -> oShowWith b oFadeIn $> Just b
     Vanish a  -> oHideWith a oFadeOut $> Nothing
-    a :=> b -> Just b <$ do
-      V2 x0 y0 <- oRead a oTranslate
+    a ::=> b -> Just b <$ do
+      V2 x0 y0 <- readGroupTrans a
       V2 x  y  <- oRead b oTranslate
-      ctx <- oRead a oContext
+      ctx <- oRead (N.head a) oContext
       oModify b (oContext .~ ctx)
-      let xt t = lerp t x0 x
-      let yt t = lerp t y0 y
-      oTween a 1 (\t -> oTranslate .~ V2 (xt t) (yt t))
-      oHide a
+      waitOn $ forM_ a $ \i ->
+        fork $ oTween i 1 $ \t ->
+          oTranslate .~ V2 (lerp t x0 x) (lerp t y0 y)
+      mapM_ oHide a
       oShow b
   ) . getCompose
+
+readGroupTrans :: Traversable t => t (Object s a) -> Scene s (V2 Double)
+readGroupTrans = sequence . (V2 <$> readCenterX <*> readCenterY)
